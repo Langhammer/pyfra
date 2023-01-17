3e9fd4b3dba1e68198e05f461c05dcfcd3c69cf9
Kay Langhammer <kaylanghammer@outlook.de>
2023-01-17 12:54:19 +0100

    Create nb for pyspark modeling

---
+++ notebooks/pyfra_nb_3_pyspark.py
@@ -0,0 +1,68 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# Notebook 3
+# ==============
+# Modelling
+
+# +
+import pandas as pd
+import numpy as np
+#uploaded = files.upload()
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
+
+
+# -
+
+# Import Dataset
+df = pd.read_csv('../data/df.csv')
+
+df = df.drop.select_dtypes(include=np.number).dropna(axis=1)
+
+# # SVM
+
+# ## With PySpark
+
+# +
+# Importing SparkSession and SParkContext
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+
+# Creating a SparkContext
+sc = SparkContext.getOrCreate()
+
+# Creating a Spark session
+spark = SparkSession \
+    .builder \
+    .appName("SVM") \
+    .getOrCreate()
+        
+spark
+
+# -
+
+# Create PySpark DataFrame from pandas DataFrame 
+df = df.reset_index(drop=True)
+sdf = spark.createDataFrame(df)
+sdf_sample = sdf.sample(False, 0.01, seed=23)
+train_sample, test_sample = sdf_sample.randomSplit([.7,.3], seed=23)
+
+from pyspark.ml.classification import LinearSVC
+svm = LinearSVC(featuresCol='features', labelCol='label',maxIter=5,predictionCol='svm_prediction')
+svm_model = svm.fit(train_sample)
+result = svm_model.transfrom(test)
