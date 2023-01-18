d0a57d4112418b1e692426abbabaa62685f8eb5b
Kay Langhammer <kaylanghammer@outlook.de>
2023-01-18 17:41:37 +0100

    Minor adjustments of nb3

--- notebooks/pyfra_nb_3.py
+++ notebooks/pyfra_nb_3.py
@@ -31,7 +31,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
-from sklearn.metrics import f1_score, balanced_accuracy_score, recall_score
+from sklearn.metrics import f1_score, accuracy_score, recall_score, make_scorer
 from imblearn.under_sampling import RandomUnderSampler
 from time import sleep
 from tqdm.notebook import tqdm
@@ -48,7 +48,7 @@ pd.testing.assert_frame_equal(left=(pd.read_csv('../data/df_check_info.csv', ind
 rus = RandomUnderSampler(random_state=23)
 
 # Create a sample of the data, because the whole dataset is too big for us to work with
-relative_sample_size = 0.1
+relative_sample_size = 0.01
 df = df.sample(frac=relative_sample_size, random_state=23)
 
 data = df.drop(columns='grav',axis=1).select_dtypes(include=np.number).dropna(axis=1)
@@ -75,13 +75,14 @@ kbest_selector.fit(X_train_scaled,y_train);
 X_train_scaled_selection = kbest_selector.transform(X_train_scaled)
 X_test_scaled_selection = kbest_selector.transform(X_test_scaled)
 print(f'We use {k_features} of the original {df.shape[1]} features')
-# -
 
-# # Applying Machine Learning Models
+# # Application of Machine Learning Models
+
+# ## Setup of Metrics Table
 
 # Creating a matrix to store the results
 
-result_metrics = pd.DataFrame(columns=['model', 'f1', 'balanced_accuracy', 'recall'], index=['lr', 'svm', 'rf', 'dt'])
+result_metrics = pd.DataFrame(columns=['model', 'f1', 'accuracy', 'recall'], index=['lr', 'svm', 'rf', 'dt'])
 result_metrics
 
 
@@ -128,3 +129,7 @@ print(cm)
 
 # + vscode={"languageId": "python"}
 print('DT Score is:',RFCLFbest.score(X_test_scaled,y_test))
+
+# # Application of Advanced Models
+
+# # Results and Conclusion
