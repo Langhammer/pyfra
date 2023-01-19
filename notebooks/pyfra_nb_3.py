457109c3cecb6d405c93436f22444f0d731ef91f
Kay Langhammer <kaylanghammer@outlook.de>
2023-01-19 14:07:42 +0100

    Remove metadata from jupytext

--- notebooks/pyfra_nb_3.py
+++ notebooks/pyfra_nb_3.py
@@ -1,16 +1,14 @@
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: -LanguageId
 #     formats: ipynb,py:light
+#     notebook_metadata_filter: -kernelspec
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
 # ---
 
 # Notebook 3
@@ -63,12 +61,11 @@ X_train, X_test, y_train, y_test  = train_test_split(data, target, test_size=0.2
 
 # # Scaling the Data and Selecting Features
 
-# + vscode={"languageId": "python"}
 std_scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled = std_scaler.transform(X_train)
 X_test_scaled = std_scaler.transform(X_test)
 
-# + vscode={"languageId": "python"}
+# +
 k_features = 25
 kbest_selector = SelectKBest(k=k_features)
 kbest_selector.fit(X_train_scaled,y_train);
@@ -145,7 +142,7 @@ result_metrics
 
 # ## Random Forest
 
-# + vscode={"languageId": "python"}
+# +
 params = {
     'criterion': ['gini'],
     'max_depth': [3,10],
@@ -159,7 +156,7 @@ RFCLF.fit(X_train_scaled,y_train)
 print(RFCLF.best_params_)
 print(RFCLF.best_score_)
 
-# + vscode={"languageId": "python"}
+# +
 RFCLFbest = GridSearchCV(DecisionTreeClassifier(),param_grid = {
     'criterion': [],
     'max_depth': [],
@@ -171,7 +168,7 @@ y_pred = RFCLFbest.predict(X_test_scaled)
 cm= pd.crosstab(y_test,y_pred, rownames=['Real'], colnames=['Prediction'])
 print(cm)
 
-# + vscode={"languageId": "python"}
+# +
 print('DT Score is:',RFCLFbest.score(X_test_scaled,y_test))
 
 # # Application of Advanced Models
