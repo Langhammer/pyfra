7892fafb4d3ba50693c998d1231d3e2c0c7c48bb
Kay Langhammer <kaylanghammer@outlook.de>
2023-02-07 18:07:11 +0100

    Add One-Hot-Encoding and Var Thresh
    The variance threshold is necessary
    because the one-hot-encoding
    introduced some features that have no variance

--- notebooks/pyfra_nb_2.py
+++ notebooks/pyfra_nb_2.py
@@ -469,6 +469,20 @@ cm["Severity"].sort_values(ascending=False)[1:]
 
 # The list shows the correlation between each variables and the target variable. Note: The decision whether a variable is important or not has to be based on the absolute value of the correlation.
 
+df = pd.get_dummies(df,columns=['daylight', 'built-up_area', 'intersection_category', 
+       'atmospheric_conditions', 'collision_category', 'Rd_Cat',
+       'Traf_Direct', 'Add_Lanes', 'Rd_Prof', 'Rd_Plan', 
+       'Rd_Cond', 'Envinmt', 'Pos_Acc', 'place', 'User_category',
+       'Sex', 'Trajectory', 'LOCP', 'ACTP', 'StateP', 'direction', 
+       'cat_veh', 'obstacle', 'obstacle_movable', 'initial_point', 'principal_maneuver',
+       'department', 'Rd_Cat', 'Traf_Direct', 'Lanes', 'Landmark',
+       'Dist_to_Landmark', 'Add_Lanes', 'Rd_Prof', 'Rd_Plan', 'Gre_Verge',
+       'Rd_Width', 'Rd_Cond', 'Envinmt', 'Pos_Acc', 'place',
+       'User_category', 'Sex', 'Trajectory', 'LOCP', 'ACTP',
+       'StateP', 'YoB', 'Security', 'direction', 'cat_veh',
+       'num_occupants', 'obstacle', 'obstacle_movable', 'initial_point',
+       'principal_maneuver', 'num_veh'])
+
 # # Export DataFrame to Pickle 
 # This step is necessary to be able to work with the data in another notebook.
 
