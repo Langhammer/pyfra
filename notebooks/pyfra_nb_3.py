2275fbf4eca2606bb208f8f4c2ce176f83f55a98
Kay Langhammer <kaylanghammer@outlook.de>
2023-02-07 18:07:55 +0100

    Change "Gravity" to "Severity"

--- notebooks/pyfra_nb_3.py
+++ notebooks/pyfra_nb_3.py
@@ -52,8 +52,8 @@ rus = RandomUnderSampler(random_state=23)
 relative_sample_size = 0.01
 df = df.sample(frac=relative_sample_size, random_state=23)
 
-data = df.drop(columns='Gravity',axis=1).select_dtypes(include=np.number).dropna(axis=1)
-target = df.Gravity
+data = df.drop(columns='Severity',axis=1).select_dtypes(include=np.number).dropna(axis=1)
+target = df['Severity']
 data, target = rus.fit_resample(X=data, y=target)
 
 target.value_counts()
